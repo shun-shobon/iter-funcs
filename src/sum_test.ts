798cbaaf6950d75b7f93cbce8d1d78aaa03903fe
Shuntaro Nishizawa <me@shun.technology>
2022-09-02 03:54:53 +0900

    test: Update `sum` test

--- src/sum_test.ts
+++ src/sum_test.ts
@@ -3,7 +3,7 @@ import { assertEquals } from "../dev_deps.ts";
 import { sum } from "./sum.ts";
 
 Deno.test("sum", () => {
-  const a = [1, 2, 3];
+  const a = [1, 2, 3, 4, 5];
 
-  assertEquals(sum(a.values()), 6);
+  assertEquals(sum(a.values()), 15);
 });
