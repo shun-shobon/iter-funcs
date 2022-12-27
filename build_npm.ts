1e98c8ac9f2fff86368eca2d483017558b9945bf
Shuntaro Nishizawa <me@shun.technology>
2022-12-27 16:32:48 +0900

    fix: Fix `main` field of `package.json`

--- build_npm.ts
+++ build_npm.ts
@@ -18,7 +18,8 @@ await build({
     description:
       "Utility functions for iterators. Inspired by Rust's `std::iter::Iterator` trait.",
     license: "MIT",
-    main: "mod.js",
+    main: "./script/mod.js",
+    unpkg: "./esm/mod.js",
     repository: {
       type: "git",
       url: "git+https://github.com/shun-shobon/iter-funcs.git",
