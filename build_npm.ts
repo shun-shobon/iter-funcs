c6529f02324841ff20ff3c1be0845210d6883f1d
Shuntaro Nishizawa <me@shun.technology>
2022-12-27 11:59:44 +0900

    build: Add dnt script

---
+++ build_npm.ts
@@ -0,0 +1,29 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.32.1/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: {
+      test: true,
+    },
+  },
+  package: {
+    name: "@shun-shobon/iter-funcs",
+    version: "0.1.0",
+    description:
+      "Utility functions for iterators. Inspired by Rust's `std::iter::Iterator` trait.",
+    license: "MIT",
+    main: "mod.js",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/shun-shobon/iter-funcs.git",
+    },
+    sideEffects: false,
+  },
+});
+
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
